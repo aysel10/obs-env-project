dbbdb64d0b8e13654dcff41d9a27f579d1add483
Aysel Abdullayeva <aysel2920@gmail.com>
2021-10-15 23:20:25 +0200

    Initial Commit

---
+++ ObsEnvProject/ObsEnvProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObsEnvProjectApp.swift
+//  ObsEnvProject
+//
+//  Created by icelkas on 15/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct ObsEnvProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
